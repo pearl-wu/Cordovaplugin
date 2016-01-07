1eb2262649d795a6e33ada8283fe05afc1a16bb5
pearl <lu92500@yahoo.com.tw>
2016-01-07 15:58:05 +0800

    Update echo-plugin.js

--- www/echo-plugin.js
+++ www/echo-plugin.js
@@ -1,11 +1,13 @@
-'use strict';
-
-var exec = require("coedova/exec")
-
-var EchoPlugin = {
-  echo: function(sendMsg, onSuccess, onFail){
-    return exec(onSuccess, onFail, 'EchoPlugin', 'echo', [sendMsg]);
-  }
-};
-
-module.exports = EchoPlugin;
+'use strict';  
+  
+var exec = require('cordova/exec');  
+  
+var EchoPlugin = {  
+  
+  echo: function(sendMsg, onSuccess, onFail) {  
+    return exec(onSuccess, onFail, 'EchoPlugin', 'echo', [sendMsg]);  
+  }  
+  
+};  
+  
+module.exports = EchoPlugin;  
